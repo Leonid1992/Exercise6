ae2c5f2b7a45d0740c8642b64719874b2a4c4b9b
Leonid <leonid.vilcu@vlc2.com>
2023-07-06 17:33:15 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+/*.6. How can you create a method that can be called without creating an instance of its class?
+
+ */
+public class Main {
+    public static void main(String[] args) {
+        Main.metodo();
+
+    }
+    public static void metodo(){
+        System.out.println("stampa dal metodo");
+    }
+
+}
